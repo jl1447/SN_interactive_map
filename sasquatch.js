67f2496e1155b63a0fe27f2cd85e16397c70fc9d
jl1447 <jordan_lineker@squamish.net>
2026-07-16 09:32:06 -0700

    Create sasquatch.js
    sasquatch javascript

---
+++ sasquatch.js
@@ -0,0 +1,93 @@
+export async function loadSasquatch(map) {
+
+    const response =
+        await fetch("./SecretSasquatch_WGS84.json");
+
+    const data =
+        await response.json();
+
+    const sasquatchIcon = L.icon({
+        iconUrl: "./images/sasquatch_rockstar.png",
+        iconSize: [48, 48],
+        iconAnchor: [24, 48],
+        popupAnchor: [0, -48]
+    });
+
+    const layer = L.geoJSON(data, {
+
+        pointToLayer(feature, latlng) {
+
+            return L.marker(latlng, {
+                icon: sasquatchIcon
+            });
+
+        },
+
+        onEachFeature(feature, layer) {
+
+            const name =
+                feature.properties?.NAME ||
+                "Unknown";
+
+            const description =
+                feature.properties?.DESCRIPTION ||
+                "";
+
+            const audioFile =
+                feature.properties?.AUDIO;
+
+            let popupContent = `
+                <div class="village-title">
+                    ${name}
+                </div>
+
+                <div>
+                    ${description}
+                </div>
+            `;
+
+            if (audioFile) {
+
+                const audioId =
+                    `audio-${feature.id}`;
+
+                popupContent += `
+                    <br>
+
+                    <button
+                        class="audio-play-btn"
+                        onclick="
+                            const audio =
+                                document.getElementById('${audioId}');
+
+                            if (audio.paused) {
+                                audio.play();
+                                this.innerText='⏸ Pause';
+                            } else {
+                                audio.pause();
+                                this.innerText='▶ Listen';
+                            }
+                        "
+                    >
+                        ▶ Listen
+                    </button>
+
+                    <audio
+                        id="${audioId}"
+                        preload="none"
+                    >
+                        ./audio/${audioFile}
+                    </audio>
+                `;
+            }
+
+            layer.bindPopup(popupContent, {
+                maxWidth: 300
+            });
+
+        }
+
+    }).addTo(map);
+
+    return layer;
+}
