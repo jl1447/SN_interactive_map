89521c36e6ceeb743d5eff49f09476feeacc5c68
jl1447 <jordan_lineker@squamish.net>
2026-07-16 12:01:55 -0700

    Update villages.js

--- villages.js
+++ villages.js
@@ -106,7 +106,7 @@ export async function loadVillages(map) {
                             preload="metadata"
                             class="popup-video"
                         >
-                            ${videoFile}video/mp4"
+                            ${videoFile}
                             >
                             Your browser does not support video.
                         </video>
