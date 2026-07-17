3e885c941239835fd0b7194feba287e76dbf43ac
jl1447 <jordan_lineker@squamish.net>
2026-07-17 16:27:26 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,8 @@
 export function createMap() {
 
-    const map = L.map("map");
+    const map = L.map("map" {
+                     zoomControl: false
+        );
 
     L.tileLayer(
         "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
@@ -10,6 +12,7 @@ export function createMap() {
     ).addTo(map);
 
     L.control.scale().addTo(map);
+    L.control.zoom({ position: 'topright' }).addTo(map);
 
     return map;
 }
