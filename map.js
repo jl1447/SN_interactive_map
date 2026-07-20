812d04d189b0f792bf9cbd5927664e1aaae075a6
jl1447 <jordan_lineker@squamish.net>
2026-07-20 10:30:15 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,9 @@
 export function createMap() {
 
-    const map = L.map("map" {
-                     zoomControl: false
-        );
+    // Fixed: Added a comma after "map", and correctly closed the options object with } before )
+    const map = L.map("map", {
+        zoomControl: false
+    });
 
     L.tileLayer(
         "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
